17e5d3d3b44a482111602c31829435d609aaeff8
Barfy007JB1 <jonathan.bathe@stfc.ac.uk>
2025-02-06 10:43:33 +0000

    Updated setup_nsights to be more secure

--- iris_gpubench/nvidia_nsight/setup_nsights.sh
+++ iris_gpubench/nvidia_nsight/setup_nsights.sh
@@ -1,19 +1,13 @@
 #! /bin/bash
 
-# Set up system
-cat /proc/sys/kernel/perf_event_paranoid
+# Set up system (changes kernel paranoid level untill reboot)
+sudo sysctl kernel.perf_event_paranoid=0
 
-sudo sh -c 'echo 2 >/proc/sys/kernel/perf_event_paranoid'
-
-sudo sh -c 'echo kernel.perf_event_paranoid=2 > /etc/sysctl.d/local.conf'
 
 # Install Nsights Package Manager for Ubuntu
-sudo apt update
-sudo apt install -y --no-install-recommends gnupg
-echo "deb http://developer.download.nvidia.com/devtools/repos/ubuntu$(source /etc/lsb-release; echo "$DISTRIB_RELEASE" | tr -d .)/$(dpkg --print-architecture) /" | tee /etc/apt/sources.list.d/nvidia-devtools.list
-sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
-sudo apt update
-sudo apt install nsight-systems-cli
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+sudo add-apt-repository "deb https://developer.download.nvidia.com/devtools/repos/ubuntu$(source /etc/lsb-release; echo "$DISTRIB_RELEASE" | tr -d .)/$(dpkg --print-architecture)/ /"
+sudo apt install nsight-systems
 
 # Check CLI is set up correctly
 
